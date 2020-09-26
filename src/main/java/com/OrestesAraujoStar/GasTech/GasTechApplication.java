01eb2fa3b90a400b7cef83c4dd80b10f19f1e64a
OrestesAraujo-Star <orestesaraujo@gmail.com>
2020-09-26 10:06:51 -0300

    Projeto Criado

---
+++ src/main/java/com/OrestesAraujoStar/GasTech/GasTechApplication.java
@@ -0,0 +1,13 @@
+package com.OrestesAraujoStar.GasTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasTechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OrestesAraujoStar/GasTech/GasTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OrestesAraujoStar.GasTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
